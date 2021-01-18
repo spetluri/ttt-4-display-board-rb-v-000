842a0838b1d952cdcc4dd8a1f4f3c299d7aa7ddd
Sai Petluri <spetluri.1@gmail.com>
2021-01-18 03:32:20 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -161,7 +161,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(output).to include(" X | X | X ")
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
     end
 
     it 'prints an entire board full of Os' do
